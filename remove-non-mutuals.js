dd6e583e2b78416bea5f3d9513374468ca15783e
shal-in <121385807+shal-in@users.noreply.github.com>
2024-11-16 11:41:49 +0000

    Update remove-non-mutuals.js

--- remove-non-mutuals.js
+++ remove-non-mutuals.js
@@ -1,1 +1,26 @@
+(() => {
+  const $moreButtons = '[aria-label="More"]';
+  const $removeFollowerButton = '[data-testid="removeFollower"]';
+  const $confirmButton = '[data-testid="confirmationSheetConfirm"]';
 
+  const retry = {
+    count: 0,
+    limit: 3,
+  };
+
+  const followersListEl = document.querySelector('[aria-label="Timeline: Followers"]');
+
+  const followersEls = followersListEl.querySelectorAll('[data-testid="UserCell"]');
+  
+  followersEls.forEach(followerEl => {
+    element = followerEl.querySelector('[aria-label^="Follow "]');
+  
+    if (element) {
+      username = element.getAttribute("aria-label").split("Follow ").pop();
+  
+      console.log(`Unfollow ${username}`);
+    } else {
+      console.log(`Do not unfollow`)
+    }
+  })
+})();
